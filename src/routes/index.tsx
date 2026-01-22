14d3e6d5826b39408fb339164322c79d542e4400
Linuss <tade3d@gmail.com>
2026-01-22 15:08:04 +0100

    Expanded index page.
    * Work in progress!
    * Add a choice sign in.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,17 @@
+import {
+  Anchor,
+  Button,
+  Card,
+  Divider,
+  Group,
+  PasswordInput,
+  Stack,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { IconBrandGithub } from "@tabler/icons-react";
 import { createFileRoute } from "@tanstack/react-router";
 import { authClient } from "~/lib/auth-client";
 
@@ -6,21 +20,76 @@ export const Route = createFileRoute("/")({
 });
 
 function RouteComponent() {
-  return (
-    <div>
-      <p>Log in:</p>
+  const form = useForm({
+    mode: "uncontrolled",
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-      <button
-        type="button"
-        onClick={async () =>
-          await authClient.signIn.social({
-            provider: "github",
-            callbackURL: "/user",
-          })
-        }
+  const handleGithubLogin = async () => {
+    await authClient.signIn.social({
+      provider: "github",
+      callbackURL: "/user",
+    });
+  };
+
+  return (
+    <Stack h={"100vh"} p={"xs"} gap={"xl"} justify="center">
+      <Title
+        order={1}
+        fz={96}
+        c={"dark"}
+        styles={{ root: { textAlign: "center" } }}
       >
-        Log in
-      </button>
-    </div>
+        agera
+      </Title>
+
+      <Stack gap={"xs"}>
+        <Group justify="space-between">
+          <Text fw={500} fz={24}>
+            Sign in
+          </Text>
+
+          <Anchor c={"dark"}>Need an account?</Anchor>
+        </Group>
+
+        <Card p={"lg"} radius={"md"} shadow="md">
+          <Stack gap={"md"}>
+            <Button
+              variant="outline"
+              color="dark"
+              leftSection={<IconBrandGithub size={18} />}
+              onClick={handleGithubLogin}
+            >
+              Sign in with GitHub
+            </Button>
+
+            <Divider label="or" />
+
+            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+              <Stack gap={"sm"}>
+                <TextInput
+                  key={form.key("email")}
+                  label="Email"
+                  {...form.getInputProps("email")}
+                />
+
+                <PasswordInput
+                  key={form.key("password")}
+                  label="Password"
+                  {...form.getInputProps("password")}
+                />
+
+                <Button type="submit" color="dark" fullWidth>
+                  Sign in
+                </Button>
+              </Stack>
+            </form>
+          </Stack>
+        </Card>
+      </Stack>
+    </Stack>
   );
 }
