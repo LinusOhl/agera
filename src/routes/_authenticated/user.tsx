e7784453f2bf9e215f01564cb5ecded7d4c01f66
Linuss <tade3d@gmail.com>
2026-01-24 12:30:51 +0100

    Removed all task creation related code.

--- src/routes/_authenticated/user.tsx
+++ src/routes/_authenticated/user.tsx
@@ -1,19 +1,14 @@
 import { Button, Center, Loader, Stack, Text, Title } from "@mantine/core";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { authClient } from "~/lib/auth-client";
-import { useCreateTaskMutation } from "~/utils/tasks/queryOptions";
 
 export const Route = createFileRoute("/_authenticated/user")({
-  loader: ({ context }) => {
-    // console.log(context.user.id);
-  },
   component: RouteComponent,
 });
 
 function RouteComponent() {
   const navigate = useNavigate();
   const { data: session, isPending } = authClient.useSession();
-  const createTaskMutation = useCreateTaskMutation();
 
   if (isPending) {
     return (
@@ -24,17 +19,7 @@ function RouteComponent() {
   }
 
   const handleLogout = async () => {
-    authClient.signOut({}, { onSuccess: () => navigate({ to: "/" }) });
-  };
-
-  const handleTaskCreation = async () => {
-    createTaskMutation.mutate({
-      data: {
-        title: "First task",
-        description: null,
-        status: "NOT_STARTED",
-      },
-    });
+    await authClient.signOut({}, { onSuccess: () => navigate({ to: "/" }) });
   };
 
   return (
