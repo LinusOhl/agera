7db2e633f63bfe47da0d0a0edd805b49f2cd320e
Linuss <tade3d@gmail.com>
2026-01-24 12:30:10 +0100

    Added constants for task statuses.

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,13 @@
 export const BASE_URL_LOCAL = "http://localhost:3000";
+
+export enum TaskStatuses {
+  NOT_STARTED = "NOT_STARTED",
+  IN_PROGRESS = "IN_PROGRESS",
+  COMPLETED = "COMPLETED",
+}
+
+export const TASK_STATUS_LABELS: Record<TaskStatuses, string> = {
+  [TaskStatuses.NOT_STARTED]: "Not started",
+  [TaskStatuses.IN_PROGRESS]: "In progress",
+  [TaskStatuses.COMPLETED]: "Completed",
+};
