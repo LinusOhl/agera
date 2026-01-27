ab1f284730b10e01f0d29b6cd30db2e3f4c87f79
Linuss <tade3d@gmail.com>
2026-01-27 12:10:26 +0100

    Created a simple loader component.

---
+++ src/components/CustomLoader.tsx
@@ -0,0 +1,9 @@
+import { Center, Loader } from "@mantine/core";
+
+export const CustomLoader = () => {
+  return (
+    <Center mih={"100vh"}>
+      <Loader color="dark" />
+    </Center>
+  );
+};
