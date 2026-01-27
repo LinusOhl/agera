c6bd89c6f1fdb31f08ec05c73e484aa24f0a38cb
Linuss <tade3d@gmail.com>
2026-01-27 10:10:55 +0100

    Updated name of task schema & type.

--- src/utils/tasks/schema.ts
+++ src/utils/tasks/schema.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 
 const Statuses = ["NOT_STARTED", "IN_PROGRESS", "COMPLETED"] as const;
 
-export const TaskCreateSchema = z.object({
+export const TaskSchema = z.object({
   title: z.string().min(1),
   description: z.string().nullable(),
   status: z.enum(Statuses),
 });
 
-export type TaskCreateType = z.infer<typeof TaskCreateSchema>;
+export type TaskType = z.infer<typeof TaskSchema>;
