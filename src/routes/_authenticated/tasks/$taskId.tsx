245ec2817d661f1b77869b6e62ca98efea20be14
Linuss <tade3d@gmail.com>
2026-01-27 08:20:59 +0100

    Added functionality to delete a task.

--- src/routes/_authenticated/tasks/$taskId.tsx
+++ src/routes/_authenticated/tasks/$taskId.tsx
@@ -1,7 +1,11 @@
-import { Badge, Button, Group, Stack, Text, Title } from "@mantine/core";
+import { Badge, Button, Group, Modal, Stack, Text, Title } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
-import { taskQueryOptions } from "~/utils/tasks/queryOptions";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import {
+  taskQueryOptions,
+  useDeleteTaskMutation,
+} from "~/utils/tasks/queryOptions";
 
 export const Route = createFileRoute("/_authenticated/tasks/$taskId")({
   loader: async ({ context, params }) => {
@@ -12,8 +16,29 @@ export const Route = createFileRoute("/_authenticated/tasks/$taskId")({
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
   const { taskId } = Route.useParams();
+
   const { data: task } = useSuspenseQuery(taskQueryOptions(taskId));
+  const deleteTaskMutation = useDeleteTaskMutation();
+
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const handleTaskDeletion = async () => {
+    deleteTaskMutation.mutate(
+      {
+        data: {
+          id: taskId,
+        },
+      },
+      {
+        onSuccess: () => {
+          close();
+          navigate({ to: "/tasks" });
+        },
+      },
+    );
+  };
 
   return (
     <Stack p={"xs"} gap={"xl"}>
@@ -44,7 +69,24 @@ function RouteComponent() {
       <Button variant="outline" color="dark">
         Edit task
       </Button>
-      <Button color="red">Delete task</Button>
+
+      <Modal opened={opened} onClose={close} title="Confirm deletion">
+        <Text mb={"md"}>Are you sure you want to delete the task?</Text>
+
+        <Group gap={"sm"} grow>
+          <Button color="red" onClick={handleTaskDeletion}>
+            Delete task
+          </Button>
+
+          <Button variant="light" color="dark" onClick={close}>
+            Cancel
+          </Button>
+        </Group>
+      </Modal>
+
+      <Button color="red" onClick={open}>
+        Delete task
+      </Button>
     </Stack>
   );
 }
