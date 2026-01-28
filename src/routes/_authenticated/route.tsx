0ecd4139aa77998fccfe234965a8923edb9708e0
Linuss <tade3d@gmail.com>
2026-01-28 11:41:05 +0100

    Added a navbar.

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,6 +1,9 @@
+import { Tabs } from "@mantine/core";
 import {
   createFileRoute,
   Outlet,
+  useLocation,
+  useNavigate,
   useRouterState,
 } from "@tanstack/react-router";
 import { CustomLoader } from "~/components/CustomLoader";
@@ -16,10 +19,30 @@ export const Route = createFileRoute("/_authenticated")({
 
 function RouteComponent() {
   const { isLoading } = useRouterState();
+  const navigate = useNavigate();
+  const pathname = useLocation({
+    select: (location) => location.pathname,
+  });
 
   if (isLoading) {
     return <CustomLoader />;
   }
 
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+
+      <Tabs
+        value={pathname}
+        variant="outline"
+        style={{ position: "fixed", bottom: 0 }}
+        onChange={(value) => navigate({ to: `${value}` })}
+      >
+        <Tabs.List>
+          <Tabs.Tab value="/tasks">Tasks</Tabs.Tab>
+          <Tabs.Tab value="/user">User</Tabs.Tab>
+        </Tabs.List>
+      </Tabs>
+    </>
+  );
 }
