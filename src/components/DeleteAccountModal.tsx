f590d2cbee82dfca630e7c636e28ca14e6ddc759
Linuss <tade3d@gmail.com>
2026-02-14 12:06:52 +0100

    Extracted 'delete account modal' to a component.

---
+++ src/components/DeleteAccountModal.tsx
@@ -0,0 +1,53 @@
+import { Button, Modal, PasswordInput, Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useNavigate } from "@tanstack/react-router";
+import { useState } from "react";
+import { authClient } from "~/lib/auth-client";
+
+export const DeleteAccountModal = () => {
+  const navigate = useNavigate();
+
+  const [password, setPassword] = useState<string>("");
+
+  const [opened, { open, close }] = useDisclosure();
+
+  const handleAccountDeletion = async () => {
+    await authClient.deleteUser(
+      {
+        password,
+        callbackURL: "/",
+      },
+      {
+        onSuccess: () => {
+          close();
+          navigate({ to: "/" });
+        },
+      },
+    );
+  };
+
+  return (
+    <>
+      <Button color="red" onClick={open}>
+        Delete account
+      </Button>
+
+      <Modal opened={opened} onClose={close} title="Delete account">
+        <Text>This will permanently delete your account. Are you sure?</Text>
+
+        <PasswordInput
+          label="Password"
+          value={password}
+          onChange={(e) => setPassword(e.currentTarget.value)}
+        />
+
+        <Button color="red" onClick={handleAccountDeletion}>
+          Delete account
+        </Button>
+        <Button color="dark" onClick={close}>
+          Cancel
+        </Button>
+      </Modal>
+    </>
+  );
+};
--- src/routes/_authenticated/user.tsx
+++ src/routes/_authenticated/user.tsx
@@ -1,15 +1,7 @@
-import {
-  Button,
-  Modal,
-  PasswordInput,
-  Stack,
-  Text,
-  Title,
-} from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { Button, Stack, Text, Title } from "@mantine/core";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { useState } from "react";
 import { CustomLoader } from "~/components/CustomLoader";
+import { DeleteAccountModal } from "~/components/DeleteAccountModal";
 import { authClient } from "~/lib/auth-client";
 
 export const Route = createFileRoute("/_authenticated/user")({
@@ -21,31 +13,12 @@ function RouteComponent() {
 
   const { data: session, isPending } = authClient.useSession();
 
-  const [password, setPassword] = useState<string>("");
-
-  const [opened, { open, close }] = useDisclosure();
-
   if (isPending) {
     return <CustomLoader />;
   }
 
   const handleLogout = async () => {
     await authClient.signOut({}, { onSuccess: () => navigate({ to: "/" }) });
-  };
-
-  const handleAccountDeletion = async () => {
-    await authClient.deleteUser(
-      {
-        password,
-        callbackURL: "/",
-      },
-      {
-        onSuccess: () => {
-          close();
-          navigate({ to: "/" });
-        },
-      },
-    );
   };
 
   return (
@@ -63,26 +36,7 @@ function RouteComponent() {
         Log out
       </Button>
 
-      <Modal opened={opened} onClose={close} title="Delete account">
-        <Text>This will permanently delete your account. Are you sure?</Text>
-
-        <PasswordInput
-          label="Password"
-          value={password}
-          onChange={(e) => setPassword(e.currentTarget.value)}
-        />
-
-        <Button color="red" onClick={handleAccountDeletion}>
-          Delete account
-        </Button>
-        <Button color="dark" onClick={close}>
-          Cancel
-        </Button>
-      </Modal>
-
-      <Button color="red" onClick={open}>
-        Delete account
-      </Button>
+      <DeleteAccountModal />
     </Stack>
   );
 }
