10bccea13f1dd99a94fd64e2a713838c4fcb3322
Linuss <tade3d@gmail.com>
2026-02-17 16:02:32 +0100

    Utilize Prisma enums instead of custom enums.

--- src/constants.ts
+++ src/constants.ts
@@ -1,27 +1,17 @@
+import { Priority, Status } from "generated/prisma/enums";
+
 export const BASE_URL_LOCAL = "http://localhost:3000";
 
-export enum TaskStatuses {
-  NOT_STARTED = "NOT_STARTED",
-  IN_PROGRESS = "IN_PROGRESS",
-  COMPLETED = "COMPLETED",
-}
-
-export const TASK_STATUS_LABELS: Record<TaskStatuses, string> = {
-  [TaskStatuses.NOT_STARTED]: "Not started",
-  [TaskStatuses.IN_PROGRESS]: "In progress",
-  [TaskStatuses.COMPLETED]: "Completed",
+export const TASK_STATUS_LABELS: Record<Status, string> = {
+  [Status.NOT_STARTED]: "Not started",
+  [Status.IN_PROGRESS]: "In progress",
+  [Status.COMPLETED]: "Completed",
 };
 
-export enum TaskPriorities {
-  LOW = "LOW",
-  MEDIUM = "MEDIUM",
-  HIGH = "HIGH",
-}
-
-export const TASK_PRIORITY_LABELS: Record<TaskPriorities, string> = {
-  [TaskPriorities.LOW]: "Low",
-  [TaskPriorities.MEDIUM]: "Medium",
-  [TaskPriorities.HIGH]: "High",
+export const TASK_PRIORITY_LABELS: Record<Priority, string> = {
+  [Priority.LOW]: "Low",
+  [Priority.MEDIUM]: "Medium",
+  [Priority.HIGH]: "High",
 };
 
 export enum SortKeys {
